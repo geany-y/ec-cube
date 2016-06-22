db5eef8d43dd9a8453316dd6fe7c17821784d314
YoshinagaYasumasa <1y.xxxsmokyxxx.y1@gmail.com>
2016-06-22 21:28:32 +0900

    deleteアクション修正

--- src/Eccube/Controller/Tutorial/CrudController.php
+++ src/Eccube/Controller/Tutorial/CrudController.php
@@ -150,7 +150,7 @@ class CrudController extends AbstractController
 
     /**
      * 削除画面
-     * 引数を元に該当レコードを削除後、エラーがあれば、LogicExceptionをスロー
+     * 引数を元に該当レコードを削除
      * 問題がなければ、登録画面に遷移
      *
      * @param Application $app
@@ -160,12 +160,17 @@ class CrudController extends AbstractController
      */
     public function delete(Application $app, Request $request, $id)
     {
-        $deleteResult = $app['eccube.repository.crud']->deleteDataById($id);
-
-        if (!$deleteResult) {
-            throw new \LogicException();
-        }
-
+        $this->isTokenValid($app);
+        $Crud = $app['orm.em']
+            ->getRepository('Eccube\Entity\Crud')
+            ->find($id);
+       if (is_null($Crud)) {
+            $app->addError('該当IDのデーターが見つかりません');
+            return $app->redirect($app->url('tutorial_crud'));
+       }
+        $app['orm.em']->remove($Crud);
+        $app['orm.em']->flush($Crud);
         return $app->redirect($app->url('tutorial_crud'));
-    }
+     }
+    
 }
