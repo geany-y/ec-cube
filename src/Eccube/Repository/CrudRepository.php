367026357572c69465a5c8c85f096b719e86e479
YoshinagaYasumasa <1y.xxxsmokyxxx.y1@gmail.com>
2016-06-22 21:08:05 +0900

    delateDataById削除

--- src/Eccube/Repository/CrudRepository.php
+++ src/Eccube/Repository/CrudRepository.php
@@ -103,24 +103,4 @@ class CrudRepository extends EntityRepository
         return true;
     }
 
-    /**
-     * deleteById
-     * dtb_crudの値をIDで引き当て、削除
-     *
-     * @param null $id
-     * @return bool|mixed
-     */
-    public function deleteDataById($id = null)
-    {
-        if (is_null($id)) {
-            return false;
-        }
-
-        $qb = $this->createQueryBuilder('dc');
-        $qb->delete()
-            ->where('dc.id = :Id')
-            ->setParameter('Id', $id);
-
-        return $qb->getQuery()->execute();
-    }
 }
